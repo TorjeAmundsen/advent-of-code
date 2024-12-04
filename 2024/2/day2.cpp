52382af3d8428610ba564cec41b7c2af392c7692
Torje Amundsen <torje.amundsen@marinehybridsolutions.no>
2024-12-04 11:13:25 +0100

    Day 2 part 2

--- 2024/2/day2.cpp
+++ 2024/2/day2.cpp
@@ -8,40 +8,50 @@ static void readFileToVector(const char* filename, std::vector<std::vector<int>>
     for (auto vector : matrix) {
         vector.clear();
     }
+
     matrix.clear();
 
     std::ifstream file(filename);
     std::string line;
+
     while (getline(file, line)) {
         std::stringstream numStringStream(line);
         std::string numString;
         std::vector<int> numsVector;
+
         while (getline(numStringStream, numString, ' ')) {
             numsVector.push_back(stoi(numString));
         }
+
         matrix.push_back(numsVector);
     }
 }
 
-int solvePart1(std::vector<std::vector<int>>& matrix) {
-    int safeReports = 0;
-
-    for (auto vector : matrix) {
-        int lastNum = vector[0];
+int isVectorSafe(std::vector<int> vector) {
+    int lastNum = vector[0];
         int lastDirection = (vector[0] - vector[1]) > 0;
         int safe = 1;
 
         for (int i = 1; i < vector.size(); ++i) {
             int direction = (lastNum - vector[i]) > 0;
             int difference = abs(lastNum - vector[i]);
+
             if (difference == 0 || difference > 3 || direction != lastDirection) {
                 safe = 0;
                 break;
             }
+
             lastNum = vector[i];
         }
 
-        safeReports += safe;
+        return safe;
+}
+
+int solvePart1(std::vector<std::vector<int>>& matrix) {
+    int safeReports = 0;
+
+    for (auto vector : matrix) {
+        safeReports += isVectorSafe(vector);
     }
 
     return safeReports;
@@ -51,20 +61,19 @@ int solvePart2(std::vector<std::vector<int>>& matrix) {
     int safeReports = 0;
 
     for (auto vector : matrix) {
-        int lastNum = vector[0];
-        int lastDirection = (vector[0] - vector[1]) > 0;
-        int unsafeties = 0;
+        for (int i = 0; i < vector.size(); ++i) {
+            std::vector<int> possibleSubvector;
 
-        for (int i = 1; i < vector.size(); ++i) {
-            int direction = (lastNum - vector[i]) > 0;
-            int difference = abs(lastNum - vector[i]);
-            if (difference == 0 || difference > 3 || direction != lastDirection) {
-                unsafeties++;
+            for (int j = 0; j < vector.size(); ++j) {
+                if (j == i) continue;
+                possibleSubvector.push_back(vector[j]);
             }
-            lastNum = vector[i];
+            
+            if (isVectorSafe(possibleSubvector)) {
+                safeReports++;
+                break;
+            }
         }
-
-        safeReports += (unsafeties < 2);
     }
 
     return safeReports;
