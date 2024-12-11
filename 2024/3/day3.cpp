699f2f36b3425699b7f7510ca9a4427bfb54c779
Torje Amundsen <torjefrafen@hotmail.no>
2024-12-11 12:39:40 +0100

    Day 3 part 1 in C++

--- 2024/3/day3.cpp
+++ 2024/3/day3.cpp
@@ -1,3 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <regex>
+
+#define INPUT_PATH "inputs/3.txt"
+
+void parseInput(const char* filename, std::vector<std::string>& inputVector) {
+    std::ifstream file(filename);
+    std::string line;
+
+    while (getline(file, line)) {
+        inputVector.push_back(line);
+    }
+}
+
+// mul(571,436)
+
+int solvePart1(std::vector<std::string>& inputVector) {
+    std::regex regex("mul\\(\\d{1,3},\\d{1,3}\\)");
+
+    int sumOfValidMultiplications = 0;
+
+    for (auto line : inputVector) {
+        auto matchedBegin = std::sregex_iterator(line.begin(), line.end(), regex);
+        auto matchedEnd = std::sregex_iterator();
+
+        for (auto i = matchedBegin; i != matchedEnd; ++i) {
+            std::string match = (*i).str();
+            size_t commaPosition = match.find(',', 5);
+            int firstNum = stoi(match.substr(4, commaPosition - 4));
+            int secondNum = stoi(match.substr(commaPosition + 1, match.size() - commaPosition - 1));
+
+            int product = firstNum * secondNum;
+
+            sumOfValidMultiplications += product;
+        }
+    }
+
+    return sumOfValidMultiplications;
+}
+
 int main() {
-    
+    std::vector<std::string> inputVector;
+
+    parseInput(INPUT_PATH, inputVector);
+
+    int part1Solution = solvePart1(inputVector);
+
+    std::cout << part1Solution << std::endl;
 }
