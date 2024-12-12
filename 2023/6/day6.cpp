13d8bebff003598d0751900b1769ae61b988c338
Torje Amundsen <torjefrafen@hotmail.no>
2024-12-12 23:00:47 +0100

    2023 Day 6 refactor progress

--- 2023/6/day6.cpp
+++ 2023/6/day6.cpp
@@ -3,12 +3,13 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <cmath>
 
 #define INPUT_PATH "inputs/6.txt"
 
 typedef struct {
-    std::vector<int> times;
-    std::vector<int> distances;
+    std::vector<std::string> times;
+    std::vector<std::string> distances;
 } ParsedInput;
 
 void parseInput(const char* filename, ParsedInput& inputStruct) {
@@ -20,7 +21,7 @@ void parseInput(const char* filename, ParsedInput& inputStruct) {
             for (int i = line.size() - 1; i >= 0; --i) {
                 const size_t startIndex = line.rfind(' ', i) + 1;
 
-                inputStruct.times.push_back(stoi(line.substr(startIndex, i - startIndex + 1)));
+                inputStruct.times.push_back(line.substr(startIndex, i - startIndex + 1));
                 i -= 6;
                 if (inputStruct.times.size() >= 4) break;
             }
@@ -28,7 +29,7 @@ void parseInput(const char* filename, ParsedInput& inputStruct) {
             for (int i = line.size() - 1; i >= 0; --i) {
                 const size_t startIndex = line.rfind(' ', i) + 1;
 
-                inputStruct.distances.push_back(stoi(line.substr(startIndex, i - startIndex + 1)));
+                inputStruct.distances.push_back(line.substr(startIndex, i - startIndex + 1));
                 i -= 6;
                 if (inputStruct.distances.size() >= 4) break;
             }
@@ -36,28 +37,57 @@ void parseInput(const char* filename, ParsedInput& inputStruct) {
     }
 }
 
-int solvePart1(ParsedInput& parsedInput) {
-    int leniencyProduct = 1;
+long long concatStringsToLong(std::vector<std::string> nums) {
+    std::string concatenaded = "";
+
+    for (auto num : nums) {
+        std::cout << num << std::endl;
+        concatenaded = num + concatenaded;
+    }
+
+    return stoll(concatenaded);
+}
+
+long long findPossibleLeniencies(long long time, long long distanceToBeat) {
+    long long left = 0;
+    long long right = time / 2;
+
+    while (left <= right) {
+        long long middle = (left + right) / 2;
+        long long timeHeld = middle / 2;
+        long long travelled = timeHeld * (time - timeHeld);
+
+        if (travelled <= distanceToBeat) {
+            left = middle + 1;
+        } else if (travelled > distanceToBeat) {
+            right = middle - 1;
+        }
+    }
+
+    return time - right;
+}
+
+long long solvePart1(ParsedInput& parsedInput) {
+    long long leniencyProduct = 1;
 
     for (int i = 0; i < parsedInput.times.size(); ++i) {
-        int currentTime = parsedInput.times[i];
-        int currentRequiredDistance = parsedInput.distances[i];
-        int leniency = 0;
-
-        for (int j = 0; j < currentTime; ++j) {
-            int currentAttempt = j * (currentTime - j);
-
-            if (currentAttempt > currentRequiredDistance) {
-                ++leniency;
-            }
-        }
-
-        leniencyProduct *= leniency;
+        long long time = stoll(parsedInput.times[i]);
+        long long distance = stoll(parsedInput.distances[i]);
+        std::cout << "Time: " << time << ", distance: " << distance;
+        long long leniencies = findPossibleLeniencies(time, distance);
+        std::cout << "\nFound leniencies: " << leniencies << std::endl;
+        leniencyProduct *= leniencies;
     }
 
     return leniencyProduct;
 }
 
+int solvePart2(ParsedInput& parsedInput) {
+    long long time = concatStringsToLong(parsedInput.times);
+    long long distance = concatStringsToLong(parsedInput.distances);
+
+    return findPossibleLeniencies(time, distance);
+}
 
 int main() {
     ParsedInput parsedInput;
@@ -67,4 +97,8 @@ int main() {
     int part1Solution = solvePart1(parsedInput);
 
     std::cout << part1Solution << std::endl;
+
+    int part2Solution = solvePart2(parsedInput);
+
+    std::cout << part2Solution << std::endl;
 }
